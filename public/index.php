f9c8ee3c8d1d82bab65468b91cc385e8e6d9900d
Xander93 <xdg1993@gmail.com>
2024-07-07 01:16:28 +0200

    symfony skeleton

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
-<html>
-<body>
-CSVtoDBPaymentProcessor
-</body>
-</html>
+<?php
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
